f5cdbc0ce1b65f1b9dcfebeb2604818c510b1d89
thienjs <thien.tran6@gmail.com>
2021-11-17 19:42:52 -0500

    showcase

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to Tennis Pickup</h1>
+      <h2>the easiest way to schedule and find local tennis matches</h2>
+    </div>
+  )
+}
